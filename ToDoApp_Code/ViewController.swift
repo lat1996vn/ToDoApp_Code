29c18dc270255c9c231f466b9eb000ca7cf35bc9
LAT <tuan.la2611@gmail.com>
2019-06-27 10:44:54 +0700

    Initial Commit

---
+++ ToDoApp_Code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp_Code
+//
+//  Created by TuanLA on 6/27/19.
+//  Copyright © 2019 TuanLA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
